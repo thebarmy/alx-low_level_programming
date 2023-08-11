515e0a404be4f0a44f2f0d284ec88f51fd37806a
thebarmy <souiyahk99@gmail.com>
2023-08-10 19:48:49 -0700

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,11 @@
 
 /**
  * main - Entry point
- * Return 1 (Success)
+ *
+ * Return : Always 1 (Success)
  */
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
-return(1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
