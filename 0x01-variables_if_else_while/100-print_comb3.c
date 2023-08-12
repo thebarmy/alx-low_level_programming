696b889d3d9c66016edb2e25f4ac15ea577d8806
thebarmy <souiyahk99@gmail.com>
2023-08-11 19:28:05 -0700

    task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Succes)
+ */
+int main(void)
+{
+int x = 5;
+char y = 'a';
+printf("the string of a digit is %c and is equal %d ", x, y);
+return (0);
+}
