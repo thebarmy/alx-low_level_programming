050101472b83cebb9911abfff55cb0b243cf5356
thebarmy <souiyahk99@gmail.com>
2023-08-12 06:28:38 -0700

    last one

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - Entry point
+ * main - Entry point 
  *
- * Return : Always 1 (Success)
- * Description : Prints a specific quote to the standart output
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
 fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+
 return (1);
 }
