40db3ab2020ded4e03649999c1fac7b67b838046
thebarmy <souiyahk99@gmail.com>
2023-08-14 17:01:23 -0700

    Do what you do better

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,13 +14,11 @@ for (m = 49; m < 58; m++)
 for (l = 50; l < 58; l++)
 {
 if (l > m && m > n)
-{          
-putchar(n);
+{putchar(n);
 putchar(m);
-putchar(l);              
+putchar(l);
 if (n != 55 || m != 56)
-{
-putchar(',');
+{putchar(',');
 putchar(' ');
 }
 }
