bd9f30e52f63d1f895458ba5aea1d10a3d102c03
thebarmy <souiyahk99@gmail.com>
2023-08-28 18:55:01 -0700

    Even more pointers

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strstr - Entry point
+ * @haystack: input
+ * @needle: input
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+for (; *haystack != '\0'; haystack++)
+{
+char *a = haystack;
+char *p = needle;
+
+while (*a == *p && *p != '\0')
+{
+a++;
+p++;
+}
+if (*p == '\0')
+return (haystack);
+}
+return (0);
+}
---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_diagsums - Entry point
+ * @a: input
+ * @size: input
+ * Return: Always 0 (Success)
+ */
+void print_diagsums(int *a, int size)
+{
+int s1, s1, x;
+
+s1 = 0;
+s2 = 0;
+
+for (x = 0; x < size; x++)
+{
+s1 = s1 + a[x * size + x];
+}
+
+for (x = size - 1; x >= 0; x--)
+{
+s2 += a[x * size + (size - x - 1)];
+}
+printf("%d, %d\n", s1, s2);
+}
