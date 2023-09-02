949a6d87571854c63ab98c19023d296297a04875
thebarmy <souiyahk99@gmail.com>
2023-09-02 16:52:07 -0700

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,62 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-/**
- * check_num - check the string
- * @str: array str
- *
- * Return: Always 0 (Success)
- */
-int check_num(char *str)
-{
-	unsigned int count;
-
-	count = 0;
-	while (count < strlen(str))
-
-	{
-		if (!isdigit(str[count]))
-		{
-			return (0);
-		}
-
-		count++;
-	}
-	return (1);
-}
 
 /**
  * main - print the name of the program
- * @argc: number arguments
- * @argv: array arguments
+ * @argc: the number of arguments
+ * @argv: array of the pointers to arguments
  *
- * Return: Always 0 ( Success)
+ * Return: Always 0.
  */
-
 int main(int argc, char *argv[])
 {
-	int count;
-	int str_to_int;
 	int sum = 0;
+	char *c;
 
-	count = 1;
-	while (count < argc)
+	while (--argc)
 	{
-		if (check_num(argv[count]))
-		{
-		 str_to_int = atoi(argv[count]);
-		 sum += str_to_int;
-		}
-		else
-		{
-		printf("Error\n");
-		return (1);
-		}
-
-		count++;
+		for (c = argv[argc]; *c; c++)
+			if (*c < '0' || *c > '9')
+				return (printf("Errr\n"), 1);
+		sum += atoi(argv[argc]);
 	}
 	printf("%d\n", sum);
-
 	return (0);
 }
